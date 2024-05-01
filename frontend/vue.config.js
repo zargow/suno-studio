74eb24c4faf3008364b9f595149fd38fa0927ba2
zargow <a@gows.us>
2024-04-30 22:24:10 -0500

    Restructure project into seperate backend and frontend directories

---
+++ frontend/vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
+});
